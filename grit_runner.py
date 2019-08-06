06b40b76c67dc8fe3979e61f86a372c996822dac
aikanor <abalsubr@ucsd.edu>
2019-08-06 12:54:33 -0700

    add new Avocado, finish jamboree sync

--- grit_runner.py
+++
@@ -1,5 +0,0 @@
-from grit.lib.multiprocessing_utils import run_in_parallel
-all_args = []
-for ct in process_data.iter_ct('all'):
-    all_args.append([ct, ['mean', 'max', 'min', 'q25']])
-run_in_parallel(8, , all_args)
